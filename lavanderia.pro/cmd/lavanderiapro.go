a35f84425542e64085b76c66e32842f52230141c
Santiago Mena <santiagomenape@gmail.com>
2023-03-31 20:48:30 -0300

    Clean space

--- lavanderia.pro/cmd/lavanderiapro.go
+++ lavanderia.pro/cmd/lavanderiapro.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	"lavanderia.pro/api"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/", getIndex)
-
-	router.Run("0.0.0.0:1002")
-}
-
-func getIndex(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Hello World!")
+	fmt.Println(api.Laundries())
 }
