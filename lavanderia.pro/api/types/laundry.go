2497af388e346e1b30cf15dce4b733912de7afcf
Santiago Mena <santiagomenape@gmail.com>
2023-04-01 12:34:21 -0300

    Add _id from mongoDB

--- lavanderia.pro/api/types/laundry.go
+++ lavanderia.pro/api/types/laundry.go
@@ -1,7 +1,7 @@
 package types
 
 type Laundry struct {
-	ID   string  `json:"id"`
+	ID   string  `json:"id" bson:"_id,omitempty"`
 	Name string  `json:"name"`
 	Lat  float64 `json:"lat"`
 	Long float64 `json:"long"`
