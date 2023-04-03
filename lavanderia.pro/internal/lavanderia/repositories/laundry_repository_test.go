1c52720cf76348f96af86cbb305a959614be88d8
Santiago Mena <santiagomenape@gmail.com>
2023-04-03 01:56:38 -0300

    Test Delete Laundry repository

--- lavanderia.pro/internal/lavanderia/repositories/laundry_repository_test.go
+++ lavanderia.pro/internal/lavanderia/repositories/laundry_repository_test.go
@@ -8,7 +8,6 @@ import (
 	"lavanderia.pro/api/types"
 	"lavanderia.pro/internal/lavanderia/config"
 	"lavanderia.pro/internal/lavanderia/databases"
-	// "os"
 	"testing"
 )
 
@@ -64,4 +63,34 @@ func TestCreateLaundry(t *testing.T) {
 	assert.Equal(t, err, nil, "Create() returns error")
 	assert.NotNil(t, laundry, "FindAllLAundries() returns nil result")
 	assert.NotEmpty(t, laundry.CreatedAt, "CreatedAt is empty")
+}
+
+func TestDeleteLaundry(t *testing.T) {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	config := config.NewConfig()
+
+	mongo := databases.NewMongoDatabase(config)
+
+	laundry, err := NewLaundryRepository(mongo).Create(&types.Laundry{
+		Name: "test",
+		Lat:  0.321,
+		Long: 0.321,
+	})
+
+	assert.Equal(t, err, nil, "Create() returns error")
+	assert.NotNil(t, laundry, "Create() returns nil result")
+	assert.NotEmpty(t, laundry.CreatedAt, "CreatedAt is empty")
+	assert.NotEmpty(t, laundry.ID, "ID is empty")
+
+	laundryDeleted, errDelete := NewLaundryRepository(mongo).Delete(&laundry)
+
+	assert.Equal(t, errDelete, nil, "Delete() returns error")
+	assert.NotNil(t, laundryDeleted, "Delete() returns nil result")
+	assert.NotEmpty(t, laundryDeleted.CreatedAt, "CreatedAt is empty")
+	assert.NotEmpty(t, laundryDeleted.DeletedAt, "DeletedAt is empty")
+	assert.NotEmpty(t, laundryDeleted.ID, "ID is empty")
+
 }
