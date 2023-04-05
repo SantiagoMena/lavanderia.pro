814f9223eab4cedde680fec56c32f2fa6f1389a8
Santiago Mena <santiagomenape@gmail.com>
2023-04-05 17:38:55 -0300

    Create multiple business

--- lavanderia.pro/internal/lavanderia/routers/routers.go
+++ lavanderia.pro/internal/lavanderia/routers/routers.go
@@ -5,7 +5,7 @@ import "go.uber.org/fx"
 var Module = fx.Invoke(
 	NewPingRouter,
 	NewGetAllBusinessRouter,
-	// NewPostBusinessRouter,
+	NewPostBusinessRouter,
 	NewDeleteBusinessRouter,
 	NewUpdateBusinessRouter,
 	NewGetBusinessRouter,
