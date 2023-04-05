6b8f3f0069dca8294345060b299759a4c5e480ae
Santiago Mena <santiagomenape@gmail.com>
2023-04-05 19:25:45 -0300

    Test: Get All Business By Auth Handler

---
+++ lavanderia.pro/internal/lavanderia/handlers/business/get_all_business_by_auth_handler_test.go
@@ -0,0 +1,90 @@
+package business
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+
+	"lavanderia.pro/internal/lavanderia/config"
+
+	"lavanderia.pro/api/types"
+	"lavanderia.pro/internal/lavanderia/databases"
+	"lavanderia.pro/internal/lavanderia/handlers/auth"
+	"lavanderia.pro/internal/lavanderia/repositories"
+	// "lavanderia.pro/internal/lavanderia/handlers/business"
+	"strings"
+	"testing"
+)
+
+func TestGetAllBusinessByAuthHandle(t *testing.T) {
+	if err := godotenv.Load("../../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	// Register
+	registerBusinessHandler := MakeRegisterBusinessToTestGetAllByAuthHandler()
+	loginHandler := MakeLoginToTestGetAllByAuthHandler()
+
+	pwd := []byte("PwD")
+
+	ti := time.Now()
+	email := []string{"new@", ti.String(), "test.com"}
+
+	auth := &types.Auth{
+		Email:    strings.Join(email, ""),
+		Password: string(pwd),
+	}
+
+	businessObj := &types.Business{
+		Name: "test register",
+		Lat:  0.321,
+		Long: 0.321,
+	}
+
+	businessRegistered, errRegister := registerBusinessHandler.Handle(auth, businessObj)
+	assert.Nil(t, errRegister, "errRegister returns not nil")
+	assert.NotEmpty(t, businessRegistered, "BusinessRegistered empty")
+
+	// Auth
+	businessLogged, errLogin := loginHandler.Handle(auth)
+	assert.Nil(t, errLogin, "errLogin returns not nil")
+
+	// Test
+	handler := MakeGetAllByAuthBusinessHandler()
+
+	allBusiness, err := handler.Handle(businessLogged.Token)
+
+	assert.Nil(t, err, "Error returns not nil")
+	assert.NotEmpty(t, allBusiness, "Business is empty")
+}
+
+func MakeGetAllByAuthBusinessHandler() *GetAllBusinessByAuthHandler {
+	config := config.NewConfig()
+	database := databases.NewMongoDatabase(config)
+	repository := repositories.NewBusinessRepository(database)
+	handler := NewGetAllBusinessByAuthHandler(repository)
+
+	return handler
+}
+
+func MakeRegisterBusinessToTestGetAllByAuthHandler() *RegisterBusinessHandler {
+	config := config.NewConfig()
+	database := databases.NewMongoDatabase(config)
+	repositoryBusiness := repositories.NewBusinessRepository(database)
+	repositoryAuth := repositories.NewAuthRepository(database, config)
+	registerBusinessHandler := NewRegisterBusinessHandler(repositoryAuth, repositoryBusiness)
+
+	return registerBusinessHandler
+}
+
+func MakeLoginToTestGetAllByAuthHandler() *auth.LoginHandler {
+	config := config.NewConfig()
+	database := databases.NewMongoDatabase(config)
+	repositoryBusiness := repositories.NewBusinessRepository(database)
+	repositoryAuth := repositories.NewAuthRepository(database, config)
+	loginHandler := auth.NewLoginHandler(repositoryAuth, repositoryBusiness)
+
+	return loginHandler
+}
