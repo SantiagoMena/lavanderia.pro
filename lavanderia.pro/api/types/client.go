996b88c8e315933943c7761d5e4ab0503bd2e55b
Santiago Mena <santiagomenape@gmail.com>
2023-04-06 18:51:39 -0300

    Type Client

---
+++ lavanderia.pro/api/types/client.go
@@ -0,0 +1,12 @@
+package types
+
+import "time"
+
+type Business struct {
+	ID        string     `json:"id" bson:"_id,omitempty" uri:"id"`
+	Name      string     `json:"name"`
+	Auth      string     `json:"auth,omitempty" bson:"auth,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty" bson:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty" bson:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" bson:"deleted_at"`
+}
