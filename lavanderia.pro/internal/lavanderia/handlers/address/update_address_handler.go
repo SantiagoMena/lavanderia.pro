d22fac5833d53b9e43b6633c93ebe451c8e98fac
Santiago Mena <santiagomenape@gmail.com>
2023-04-07 12:26:31 -0300

    Update AddressHandler

---
+++ lavanderia.pro/internal/lavanderia/handlers/address/update_address_handler.go
@@ -0,0 +1,22 @@
+package address
+
+import (
+	"lavanderia.pro/api/types"
+	"lavanderia.pro/internal/lavanderia/repositories"
+)
+
+type UpdateAddressHandler struct {
+	repository *repositories.AddressRepository
+}
+
+func NewUpdateAddressHandler(repository *repositories.AddressRepository) *UpdateAddressHandler {
+	return &UpdateAddressHandler{
+		repository: repository,
+	}
+}
+
+func (ch *UpdateAddressHandler) Handle(address *types.Address) (types.Address, error) {
+	addressUpdated, err := ch.repository.Update(address)
+
+	return addressUpdated, err
+}
