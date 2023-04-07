96536dd55899b7c319b2ad4da79be1def1b97569
Santiago Mena <santiagomenape@gmail.com>
2023-04-06 22:00:34 -0300

    Test Get Client Controller

--- lavanderia.pro/internal/lavanderia/controllers/client_controller_test.go
+++ lavanderia.pro/internal/lavanderia/controllers/client_controller_test.go
@@ -3,6 +3,10 @@ package controllers
 import (
 	"fmt"
 
+	"strings"
+	"testing"
+	"time"
+
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
@@ -13,9 +17,6 @@ import (
 	"lavanderia.pro/internal/lavanderia/handlers/business"
 	"lavanderia.pro/internal/lavanderia/handlers/client"
 	"lavanderia.pro/internal/lavanderia/repositories"
-	"strings"
-	"testing"
-	"time"
 )
 
 func TestRegisterClient(t *testing.T) {
@@ -41,98 +42,49 @@ func TestRegisterClient(t *testing.T) {
 		Name: "test register",
 	}
 
-	business, err := controllerClient.RegisterClient(auth, clientObj)
+	client, err := controllerClient.RegisterClient(auth, clientObj)
 
 	assert.Nil(t, err, "Error returns not nil")
-	assert.NotEmpty(t, business, "Business is empty")
-	assert.NotEmpty(t, business.ID, "Business ID is empty")
+	assert.NotEmpty(t, client, "Client is empty")
+	assert.NotEmpty(t, client.ID, "Client ID is empty")
 }
 
-// func TestDeleteBusiness(t *testing.T) {
-// 	if err := godotenv.Load("../../../.env.test"); err != nil {
-// 		fmt.Println("No .env.test file found")
-// 	}
-// 	controller := MakeController()
+func TestGetClient(t *testing.T) {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+	// controllerAuth := MakeAuthControllerForTest()
+	controllerClient := MakeClientControllerForTest()
 
-// 	business, err := controller.PostBusiness(&types.Business{
-// 		Name: "test",
-// 		Lat:  0.123,
-// 		Long: 0.123,
-// 	})
+	pwd := []byte("PwD")
+	password, errPass := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
 
-// 	assert.Nil(t, err, "Error returns not nil on create business to delete")
-// 	assert.NotEmpty(t, business, "Business is empty on create business to delete")
-// 	assert.NotEmpty(t, business.ID, "Business ID is empty on create business to delete")
+	assert.Equal(t, errPass, nil, "GenerateFromPassword() returns error")
+	ti := time.Now()
+	email := []string{"new@", ti.String(), "test.com"}
 
-// 	businessDeleted, errDelete := controller.DeleteBusiness(&business)
-// 	assert.Nil(t, errDelete, "Error returns not nil on delete business")
-// 	assert.NotEmpty(t, businessDeleted, "Business is empty on delete business")
-// 	assert.NotEmpty(t, businessDeleted.ID, "Business ID is empty on delete business")
-// 	assert.NotEmpty(t, businessDeleted.DeletedAt, "Business DeletedAt is empty on delete business")
-// }
+	auth := &types.Auth{
+		Email:    strings.Join(email, ""),
+		Password: string(password),
+	}
 
-// func TestUpdateBusiness(t *testing.T) {
-// 	if err := godotenv.Load("../../../.env.test"); err != nil {
-// 		fmt.Println("No .env.test file found")
-// 	}
-// 	controller := MakeController()
+	clientObj := &types.Client{
+		Name: "test register",
+	}
 
-// 	business, err := controller.PostBusiness(&types.Business{
-// 		Name: "test",
-// 		Lat:  0.123,
-// 		Long: 0.123,
-// 	})
+	client, err := controllerClient.RegisterClient(auth, clientObj)
 
-// 	assert.Nil(t, err, "Error returns not nil on create business to delete")
-// 	assert.NotEmpty(t, business, "Business is empty on create business to delete")
-// 	assert.NotEmpty(t, business.ID, "Business ID is empty on create business to delete")
-// 	assert.Equal(t, "test", business.Name, "Name not saved properly")
-// 	assert.Equal(t, 0.123, business.Lat, "Lat not saved properly")
-// 	assert.Equal(t, 0.123, business.Long, "Long not saved properly")
+	assert.Nil(t, err, "Error returns not nil")
+	assert.NotEmpty(t, client, "Client is empty")
+	assert.NotEmpty(t, client.ID, "Client ID is empty")
 
-// 	businessUpdated, errUpdate := controller.UpdateBusiness(&types.Business{
-// 		ID:   business.ID,
-// 		Name: "updated",
-// 		Lat:  0.321,
-// 		Long: 0.321,
-// 	})
-// 	assert.Nil(t, errUpdate, "Error returns not nil on delete business")
-// 	assert.NotEmpty(t, businessUpdated, "Business is empty on delete business")
-// 	assert.NotEmpty(t, businessUpdated.ID, "Business ID is empty on delete business")
-// 	assert.NotEmpty(t, businessUpdated.UpdatedAt, "Business UpdatedAt is empty on delete business")
-// 	assert.Equal(t, "updated", businessUpdated.Name, "Name not save properly")
-// 	assert.Equal(t, 0.321, businessUpdated.Lat, "Lat not save properly")
-// 	assert.Equal(t, 0.321, businessUpdated.Long, "Long not save properly")
-// }
-
-// func TestGetBusiness(t *testing.T) {
-// 	if err := godotenv.Load("../../../.env.test"); err != nil {
-// 		fmt.Println("No .env.test file found")
-// 	}
-// 	controller := MakeController()
-
-// 	business, err := controller.PostBusiness(&types.Business{
-// 		Name: "test",
-// 		Lat:  0.123,
-// 		Long: 0.123,
-// 	})
-
-// 	assert.Nil(t, err, "Error returns not nil on create business to delete")
-// 	assert.NotEmpty(t, business, "Business is empty on create business to delete")
-// 	assert.NotEmpty(t, business.ID, "Business ID is empty on create business to delete")
-// 	assert.Equal(t, "test", business.Name, "Name not saved properly")
-// 	assert.Equal(t, 0.123, business.Lat, "Lat not saved properly")
-// 	assert.Equal(t, 0.123, business.Long, "Long not saved properly")
-
-// 	businessGotten, errGet := controller.GetBusiness(&business)
-// 	assert.Nil(t, errGet, "Error returns not nil on delete business")
-// 	assert.NotEmpty(t, businessGotten, "Business is empty on delete business")
-// 	assert.NotEmpty(t, businessGotten.ID, "Business ID is empty on delete business")
-// 	assert.NotEmpty(t, businessGotten.CreatedAt, "Business CreatedAt is empty on delete business")
-// 	assert.Equal(t, "test", businessGotten.Name, "Name not get properly")
-// 	assert.Equal(t, 0.123, businessGotten.Lat, "Lat not get properly")
-// 	assert.Equal(t, 0.123, businessGotten.Long, "Long not get properly")
-// }
+	clientGotten, errGet := controllerClient.GetClient(&client)
+	assert.Nil(t, errGet, "Error returns not nil on delete client")
+	assert.NotEmpty(t, clientGotten, "Client is empty on delete client")
+	assert.NotEmpty(t, clientGotten.ID, "Client ID is empty on delete client")
+	assert.NotEmpty(t, clientGotten.CreatedAt, "Client CreatedAt is empty on delete client")
+	assert.Equal(t, "test register", clientGotten.Name, "Name not get properly")
+}
 
 func MakeClientControllerForTest() *ClientController {
 	config := config.NewConfig()
