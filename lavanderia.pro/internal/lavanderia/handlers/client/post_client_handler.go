ac79d52aa21901f77d8324d063c2f11609db8222
Santiago Mena <santiagomenape@gmail.com>
2023-04-07 00:01:32 -0300

    Post Client

---
+++ lavanderia.pro/internal/lavanderia/handlers/client/post_client_handler.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"errors"
+
+	"lavanderia.pro/api/types"
+	"lavanderia.pro/internal/lavanderia/repositories"
+)
+
+type PostClientHandler struct {
+	repository *repositories.ClientRepository
+}
+
+func NewPostClientHandler(clientRepository *repositories.ClientRepository) *PostClientHandler {
+	return &PostClientHandler{
+		repository: clientRepository,
+	}
+}
+
+func (ch *PostClientHandler) PostClient(client *types.Client) (*types.Client, error) {
+	// find client by auth
+	clientFound, _ := ch.repository.GetClientByAuth(client)
+	emptyClient := types.Client{}
+
+	if clientFound != emptyClient {
+		return &types.Client{}, errors.New("client already registered")
+	}
+
+	// if exists error
+
+	// if not create
+
+	// return client
+
+	return &types.Client{}, nil
+}
---
+++ lavanderia.pro/internal/lavanderia/handlers/client/post_client_handler_test.go
@@ -0,0 +1,71 @@
+package client
+
+import (
+	"github.com/joho/godotenv"
+	"lavanderia.pro/internal/lavanderia/config"
+	"lavanderia.pro/internal/lavanderia/databases"
+
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
+	"lavanderia.pro/api/types"
+	"lavanderia.pro/internal/lavanderia/repositories"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestPostClient(t *testing.T) {
+	if err := godotenv.Load("../../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	handler := MakePostClientHandlerToTest()
+
+	createHandler := MakeClientRegisterHandlerToTestPostClient()
+
+	pwd := []byte("PwD")
+	password, errPass := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+
+	assert.Equal(t, errPass, nil, "GenerateFromPassword() returns error")
+	ti := time.Now()
+	email := []string{"new@", ti.String(), "test.com"}
+
+	auth := &types.Auth{
+		Email:    strings.Join(email, ""),
+		Password: string(password),
+	}
+
+	clientObj := &types.Client{
+		Name: "test register",
+	}
+
+	clientRegistered, errRegister := createHandler.Handle(auth, clientObj)
+
+	assert.Nil(t, errRegister, "Error on register client")
+	assert.NotEmpty(t, clientRegistered, "Client is empty on register")
+
+	clientPosted, errPost := handler.PostClient(&clientRegistered)
+
+	assert.NotNil(t, errPost, "Error on post client")
+	assert.Empty(t, clientPosted, "Client Posted Empty")
+}
+
+func MakePostClientHandlerToTest() *PostClientHandler {
+	config := config.NewConfig()
+	mongo := databases.NewMongoDatabase(config)
+	repository := repositories.NewClientRepository(mongo)
+	handler := NewPostClientHandler(repository)
+
+	return handler
+}
+
+func MakeClientRegisterHandlerToTestPostClient() *RegisterClientHandler {
+	config := config.NewConfig()
+	database := databases.NewMongoDatabase(config)
+	repositoryAuth := repositories.NewAuthRepository(database, config)
+	repositoryClient := repositories.NewClientRepository(database)
+	handler := NewRegisterClientHandler(repositoryAuth, repositoryClient)
+
+	return handler
+}
