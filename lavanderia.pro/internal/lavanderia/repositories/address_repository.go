52b77abb867f03e8bfab37640fde7b1effd7c457
Santiago Mena <santiagomenape@gmail.com>
2023-04-07 12:20:52 -0300

    Update Adress Repository

--- lavanderia.pro/internal/lavanderia/repositories/address_repository.go
+++ lavanderia.pro/internal/lavanderia/repositories/address_repository.go
@@ -1,7 +1,7 @@
 package repositories
 
 import (
-	"fmt"
+	"errors"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
@@ -73,13 +73,59 @@ func (addressRepository *AddressRepository) Get(address *types.Address) (*types.
 	if err != nil {
 		return &types.Address{}, err
 	}
-	fmt.Println("objectAddress")
-	fmt.Println(objectAddress)
+
 	var foundAddress types.Address
 
 	objectUpdt, _ := bson.Marshal(objectAddress)
 	bson.Unmarshal(objectUpdt, &foundAddress)
-	fmt.Println("foundAddress")
-	fmt.Println(foundAddress)
+
 	return &foundAddress, nil
+}
+
+func (addressRepository *AddressRepository) Update(address *types.Address) (types.Address, error) {
+	id, _ := primitive.ObjectIDFromHex(address.ID)
+	t := time.Now()
+	address.UpdatedAt = &t
+
+	filter := bson.D{
+		{Key: "_id", Value: id},
+		{Key: "deleted_at", Value: nil},
+	}
+
+	objectAddress, errFind := addressRepository.database.FindOne(addressCollection, filter)
+
+	var foundAddress types.Address
+	objectUpdt, _ := bson.Marshal(objectAddress)
+	bson.Unmarshal(objectUpdt, &foundAddress)
+	if errFind != nil {
+		return types.Address{}, errors.New("object not found")
+	}
+
+	update := bson.D{{"$set", bson.D{
+		{Key: "client", Value: address.Client},
+		{Key: "name", Value: address.Name},
+		{Key: "position", Value: address.Position},
+		{Key: "address", Value: address.Address},
+		{Key: "phone", Value: address.Phone},
+		{Key: "extra", Value: address.Extra},
+		{Key: "updated_at", Value: address.UpdatedAt},
+	}}}
+
+	addressUpdatedMongo, errUpdate := addressRepository.database.UpdateOne(addressCollection, filter, update)
+
+	if errUpdate != nil {
+		return types.Address{}, errors.New("error on update object")
+	}
+
+	var addressUpdated types.Address
+	addressMarshal, _ := bson.Marshal(addressUpdatedMongo)
+	bson.Unmarshal(addressMarshal, &addressUpdated)
+
+	adressFound, errFind := addressRepository.database.UpdateOne(addressCollection, filter, update)
+
+	var addressFoundUnmarshal types.Address
+	addressFoundMarshal, _ := bson.Marshal(adressFound)
+	bson.Unmarshal(addressFoundMarshal, &addressFoundUnmarshal)
+
+	return addressFoundUnmarshal, errFind
 }
