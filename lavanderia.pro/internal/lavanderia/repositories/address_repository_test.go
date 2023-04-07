cbb57c25d18449e2d09265fb3825ab16106f9595
Santiago Mena <santiagomenape@gmail.com>
2023-04-07 05:16:10 -0300

    Address Repository

---
+++ lavanderia.pro/internal/lavanderia/repositories/address_repository_test.go
@@ -0,0 +1,38 @@
+package repositories
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"lavanderia.pro/api/types"
+	"lavanderia.pro/internal/lavanderia/config"
+	"lavanderia.pro/internal/lavanderia/databases"
+	"testing"
+)
+
+func TestCreateAddress(t *testing.T) {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	addressRepository := MakeAddressRepositoryToTest()
+
+	address, err := addressRepository.Create(&types.Address{
+		Position: []float64{-71.327767, -41.138444},
+		Name:     "House",
+		Extra:    "Call me",
+		Address:  "Av. Pioneros 200, S.C Bariloche, Argentina",
+	})
+
+	assert.Equal(t, err, nil, "Create() returns error")
+	assert.NotNil(t, address, "FindAllBusiness() returns nil result")
+	assert.NotEmpty(t, address.CreatedAt, "CreatedAt is empty")
+}
+
+func MakeAddressRepositoryToTest() *AddressRepository {
+	config := config.NewConfig()
+	mongo := databases.NewMongoDatabase(config)
+	addressRepository := NewAddressRepository(mongo)
+
+	return addressRepository
+}
