2d5dc07328e5585702b9ef67023b5fdfe0699d10
Santiago Mena <santiagomenape@gmail.com>
2023-04-08 17:35:27 -0300

    Get Delivery controller

--- lavanderia.pro/internal/lavanderia/controllers/delivery_controller.go
+++ lavanderia.pro/internal/lavanderia/controllers/delivery_controller.go
@@ -7,18 +7,31 @@ import (
 
 type DeliveryController struct {
 	PostDeliveryHandler *delivery.PostDeliveryHandler
+	GetDeliveryHandler  *delivery.GetDeliveryHandler
 }
 
 func NewDeliveryController(
 	PostDeliveryHandler *delivery.PostDeliveryHandler,
+	GetDeliveryHandler *delivery.GetDeliveryHandler,
 ) *DeliveryController {
 	return &DeliveryController{
 		PostDeliveryHandler: PostDeliveryHandler,
+		GetDeliveryHandler:  GetDeliveryHandler,
 	}
 }
 
 func (controller DeliveryController) PostDelivery(delivery *types.Delivery) (types.Delivery, error) {
 	deliveryDb, err := controller.PostDeliveryHandler.Handle(delivery)
+
+	if err != nil {
+		return types.Delivery{}, err
+	}
+
+	return deliveryDb, err
+}
+
+func (controller DeliveryController) GetDeliveryByAuth(delivery *types.Delivery) (types.Delivery, error) {
+	deliveryDb, err := controller.GetDeliveryHandler.Handle(delivery)
 
 	if err != nil {
 		return types.Delivery{}, err
