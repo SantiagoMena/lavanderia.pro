ee7aab42277d981f2b1894b1c587ffc4c22bf728
Santiago Mena <santiagomenape@gmail.com>
2023-04-08 16:31:41 -0300

    Get Delivery By Auth

--- lavanderia.pro/internal/lavanderia/repositories/delivery_repository_test.go
+++ lavanderia.pro/internal/lavanderia/repositories/delivery_repository_test.go
@@ -27,3 +27,27 @@ func TestCreateDelivery(t *testing.T) {
 	assert.NotNil(t, delivery, "FindAllDelivery() returns nil result")
 	assert.NotEmpty(t, delivery.CreatedAt, "CreatedAt is empty")
 }
+
+func TestGetDeliveryByAuth(t *testing.T) {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	config := config.NewConfig()
+	mongo := databases.NewMongoDatabase(config)
+	repository := NewDeliveryRepository(mongo)
+
+	deliveryCreated, errCreate := repository.Create(&types.Delivery{
+		Name: "TEST",
+	})
+
+	assert.Equal(t, errCreate, nil, "Error on create() delivery")
+	assert.NotNil(t, deliveryCreated, "Create() returns nil result")
+	assert.NotEmpty(t, deliveryCreated.CreatedAt, "CreatedAt is empty")
+	assert.NotEmpty(t, deliveryCreated.ID, "ID created is empty")
+
+	deliveryFound, errFind := repository.GetDeliveryByAuth(&deliveryCreated)
+
+	assert.Equal(t, errFind, nil, "Error on GetDelivery() delivery")
+	assert.NotNil(t, deliveryFound, "GetDelivery() returns nil result")
+}
