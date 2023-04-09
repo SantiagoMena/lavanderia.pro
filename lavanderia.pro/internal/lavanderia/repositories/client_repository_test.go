d154ba7c360895ed6affb8d8cee39aba171424f4
Santiago Mena <santiagomenape@gmail.com>
2023-04-08 21:33:00 -0300

    Update Client TEST

--- lavanderia.pro/internal/lavanderia/repositories/client_repository_test.go
+++ lavanderia.pro/internal/lavanderia/repositories/client_repository_test.go
@@ -54,4 +54,27 @@ func TestGetClient(t *testing.T) {
 	// assert.Equal(t, "TEST", clientFound.Name, "Error on GetClient() client")
 }
 
-// TODO: Test update client
+func TestUpdateClient(t *testing.T) {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	config := config.NewConfig()
+	mongo := databases.NewMongoDatabase(config)
+
+	client, err := NewClientRepository(mongo).Create(&types.Client{
+		Name: "test",
+	})
+
+	assert.Equal(t, err, nil, "Create() returns error")
+	assert.NotNil(t, client, "FindAllClient() returns nil result")
+	assert.NotEmpty(t, client.CreatedAt, "CreatedAt is empty")
+
+	client.Name = "UPDATED"
+	clientUpdated, errUpdate := NewClientRepository(mongo).Update(&client)
+
+	assert.Equal(t, errUpdate, nil, "Create() returns error")
+	assert.NotNil(t, clientUpdated, "FindAllClient() returns nil result")
+	assert.NotEmpty(t, clientUpdated.UpdatedAt, "UpdatedAt is empty")
+	assert.Equal(t, "UPDATED", clientUpdated.Name, "client name not updated properly")
+}
