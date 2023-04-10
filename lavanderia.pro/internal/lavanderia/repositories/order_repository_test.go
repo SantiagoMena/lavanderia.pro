ce3c7c158d492582d7947a492fa97c45b6a4ce51
Santiago Mena <santiagomenape@gmail.com>
2023-04-10 02:26:07 -0300

    pickup business order test repository

--- lavanderia.pro/internal/lavanderia/repositories/order_repository_test.go
+++ lavanderia.pro/internal/lavanderia/repositories/order_repository_test.go
@@ -1670,10 +1670,197 @@ func TestAssignDeliveryOrder(t *testing.T) {
 			Name: "Test Delivery Person",
 		},
 	})
-	fmt.Println(orderFinished.ID)
+
 	assert.Equal(t, nil, errAssignDelivery, "error on assign delivery order")
 	assert.NotEmpty(t, orderAssignedDelivery, "error on assign delivery order")
 	assert.NotEmpty(t, orderAssignedDelivery.AssignedDeliveryAt, "error on accept order AssignedDeliveryAt empty")
+
+}
+
+func TestPickUpBusinessOrder(t *testing.T) {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		fmt.Println("No .env.test file found")
+	}
+
+	authRepository := MakeAuthRepositoryForTestOrder()
+	businessRepository := MakeBusinessRepositoryTestOrder()
+	deliveryRepository := MakeDeliveryRepositoryTestOrder()
+	clientRepository := MakeClientRepositoryTestOrder()
+	addressRepository := MakeAddressRepositoryTestOrder()
+	productRepository := MakeProductRepositoryTestOrder()
+	orderRepository := MakeOrderRepositoryTestOrder()
+
+	// Register Business
+	pwd := []byte("PwD")
+	password, errPass := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+
+	assert.Equal(t, errPass, nil, "GenerateFromPassword() returns error")
+
+	email := []string{"new@", MakeRandomString(7), "test.com"}
+
+	authBusiness, errAuthBusiness := authRepository.Create(&types.Auth{
+		Email:    strings.Join(email, ""),
+		Password: string(password),
+	})
+
+	assert.Equal(t, nil, errAuthBusiness, "error on create business auth")
+	assert.NotEmpty(t, authBusiness, "error on create business auth")
+
+	business, errBussiness := businessRepository.Create(&types.Business{
+		Auth: authBusiness.ID,
+		Name: "Test",
+		Position: types.Geometry{
+			Type:        "Point",
+			Coordinates: []float64{-71.327767, -41.138444},
+		},
+	})
+
+	assert.Equal(t, nil, errBussiness, "error on create business")
+	assert.NotEmpty(t, business, "error on create business")
+
+	// Register Delivery
+	passwordDelivery, errPassDelivery := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+
+	assert.Equal(t, errPassDelivery, nil, "GenerateFromPassword() returns error")
+
+	emailDelivery := []string{"new@", MakeRandomString(7), "test.com"}
+
+	authDelivery, errAuthDelivery := authRepository.Create(&types.Auth{
+		Email:    strings.Join(emailDelivery, ""),
+		Password: string(passwordDelivery),
+	})
+
+	assert.Equal(t, nil, errAuthDelivery, "error on create delivery auth")
+	assert.NotEmpty(t, authDelivery, "error on create delivery auth")
+
+	delivery, errDelivery := deliveryRepository.Create(&types.Delivery{
+		Name: "Test",
+		Auth: authDelivery.ID,
+	})
+
+	assert.Equal(t, nil, errDelivery, "error on create delivery")
+	assert.NotEmpty(t, delivery, "error on create delivery")
+	assert.NotEmpty(t, delivery.CreatedAt, "error on create delivery CreatedAt empty")
+
+	// Register Client
+	passwordClient, errPassClient := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+
+	assert.Equal(t, errPassClient, nil, "GenerateFromPassword() returns error")
+
+	emailClient := []string{"new@", MakeRandomString(7), "test.com"}
+
+	authClient, errAuthClient := authRepository.Create(&types.Auth{
+		Email:    strings.Join(emailClient, ""),
+		Password: string(passwordClient),
+	})
+
+	assert.Equal(t, nil, errAuthClient, "error on create client auth")
+	assert.NotEmpty(t, authClient, "error on create client auth")
+
+	client, errClient := clientRepository.Create(&types.Client{
+		Name: "Test",
+		Auth: authClient.ID,
+	})
+
+	assert.Equal(t, nil, errClient, "error on create client")
+	assert.NotEmpty(t, client, "error on create client")
+
+	// Create Address
+	address, errAddress := addressRepository.Create(&types.Address{
+		Client: client.ID,
+		Position: types.Geometry{
+			Type:        "Point",
+			Coordinates: []float64{-71.327767, -41.138444},
+		},
+		Name:    "TEST_ORDER",
+		Extra:   "Call me",
+		Phone:   "+123123123",
+		Address: "Av. Pioneros 201, S.C Bariloche, Argentina",
+	})
+
+	assert.Equal(t, nil, errAddress, "error on create address")
+	assert.NotEmpty(t, address, "error on create address")
+	assert.NotEmpty(t, address.CreatedAt, "error on create address CreatedAt empty")
+
+	// Make Product
+	product, errProduct := productRepository.Create(&types.Product{
+		Name:     "test_product",
+		Price:    0.123,
+		Business: business.ID,
+	})
+
+	assert.Equal(t, nil, errProduct, "error on create product")
+	assert.NotEmpty(t, product, "error on create product")
+
+	// Create product list
+	productList := &[]types.OrderProduct{
+		{Product: product, Amount: 7},
+	}
+
+	// Create Order
+	order, errOrder := orderRepository.Create(&types.Order{
+		Products: *productList,
+		Client:   client,
+		Business: business,
+		Delivery: delivery,
+	})
+
+	assert.Equal(t, nil, errOrder, "error on create order")
+	assert.NotEmpty(t, order, "error on create order")
+
+	orderAccepted, errFind := orderRepository.Accept(&types.Order{ID: order.ID})
+
+	assert.Equal(t, nil, errFind, "error on accept order")
+	assert.NotEmpty(t, orderAccepted, "error on accept order")
+	assert.NotEmpty(t, orderAccepted.AcceptedAt, "error on accept order AcceptedAt empty")
+
+	orderAssignPickUp, errAssignPickUp := orderRepository.AssignPickUp(&types.Order{
+		ID: order.ID,
+		PickUp: types.Delivery{
+			Name: "Test Delivery Person",
+		},
+	})
+
+	assert.Equal(t, nil, errAssignPickUp, "error on assign pickup order")
+	assert.NotEmpty(t, orderAssignPickUp, "error on assign pickup order")
+	assert.NotEmpty(t, orderAssignPickUp.AssignedPickUpAt, "error on accept order AssignedPickUpAt empty")
+
+	orderPicketUp, errPickUp := orderRepository.PickUpClient(&types.Order{ID: orderAssignPickUp.ID})
+
+	assert.Equal(t, nil, errPickUp, "error on pickup order")
+	assert.NotEmpty(t, orderPicketUp, "error on pickup order")
+	assert.NotEmpty(t, orderPicketUp.PickUpClientAt, "error on pickup order PickUpClientAt empty")
+
+	orderProcessed, errProcess := orderRepository.Process(&types.Order{ID: orderAssignPickUp.ID})
+
+	assert.Equal(t, nil, errProcess, "error on process pickup order")
+	assert.NotEmpty(t, orderProcessed, "error on process pickup order")
+	assert.NotEmpty(t, orderProcessed.ProcessingAt, "error on accept order ProcessingAt empty")
+
+	orderFinished, errFinish := orderRepository.Finish(&types.Order{ID: orderAssignPickUp.ID})
+
+	assert.Equal(t, nil, errFinish, "error on finish pickup order")
+	assert.NotEmpty(t, orderFinished, "error on finish pickup order")
+	assert.NotEmpty(t, orderFinished.FinishedAt, "error on accept order FinishedAt empty")
+
+	orderAssignedDelivery, errAssignDelivery := orderRepository.AssignDelivery(&types.Order{
+		ID: orderFinished.ID,
+		Delivery: types.Delivery{
+			Name: "Test Delivery Person",
+		},
+	})
+
+	assert.Equal(t, nil, errAssignDelivery, "error on assign delivery order")
+	assert.NotEmpty(t, orderAssignedDelivery, "error on assign delivery order")
+	assert.NotEmpty(t, orderAssignedDelivery.AssignedDeliveryAt, "error on accept order AssignedDeliveryAt empty")
+
+	orderPicketUpBusiness, errPickUpBusiness := orderRepository.PickUpBusiness(&types.Order{
+		ID: orderFinished.ID,
+	})
+
+	assert.Equal(t, nil, errPickUpBusiness, "error on pickup business order")
+	assert.NotEmpty(t, orderPicketUpBusiness, "error on pickup business order")
+	assert.NotEmpty(t, orderPicketUpBusiness.PickUpBusinessAt, "error on pickup business order PickUpBusinessAt empty")
 
 }
 
