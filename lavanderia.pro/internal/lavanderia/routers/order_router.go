43294ff2af68f02f4aa08bbdb251078877b4d48d
Santiago Mena <santiagomenape@gmail.com>
2023-04-10 02:13:35 -0300

    assign delivery order router

--- lavanderia.pro/internal/lavanderia/routers/order_router.go
+++ lavanderia.pro/internal/lavanderia/routers/order_router.go
@@ -422,3 +422,47 @@ func NewFinishOrderRouter(
 		c.IndentedJSON(http.StatusOK, orderFinished)
 	})
 }
+
+func NewAssignDeliveryOrderRouter(
+	r *gin.Engine,
+	productController *controllers.ProductController,
+	orderController *controllers.OrderController,
+	businessController *controllers.BusinessController,
+) {
+	r.POST("/business-order/assign-delivery/:id", func(c *gin.Context) {
+		authId := c.MustGet("auth")
+
+		var orderId types.Order
+		if err := c.ShouldBindUri(&orderId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err})
+			return
+		}
+
+		if authId == nil {
+			c.JSON(http.StatusForbidden, gin.H{"msg": "permissions denied"})
+			return
+		}
+
+		// Handle Controller
+		order, err := orderController.GetOrder(&orderId)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+			return
+		}
+
+		if order.Client.Auth != authId.(string) {
+			c.JSON(http.StatusForbidden, gin.H{"msg": "permissions denied"})
+			return
+		}
+
+		orderFinished, errFinish := orderController.AssignDeliveryOrder(&order)
+
+		if errFinish != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": "error on assign delivery order"})
+			return
+		}
+
+		c.IndentedJSON(http.StatusOK, orderFinished)
+	})
+}
--- lavanderia.pro/internal/lavanderia/routers/routers.go
+++ lavanderia.pro/internal/lavanderia/routers/routers.go
@@ -39,4 +39,5 @@ var Module = fx.Invoke(
 	NewPickUpOrderRouter,
 	NewProcessOrderRouter,
 	NewFinishOrderRouter,
+	NewAssignDeliveryOrderRouter,
 )
