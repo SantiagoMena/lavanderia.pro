b1b4fd9e7826115d0a151ea5d65bc0df03ec0bdb
Santiago Mena <santiagomenape@gmail.com>
2023-04-27 20:54:16 -0300

    Green Template

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lavanderia.pro',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const LoginPage(title: 'Login'),
     );
