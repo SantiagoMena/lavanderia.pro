dedfc7224289cb2e7401bebc66605ca747ed5a35
Santiago Mena <santiagomenape@gmail.com>
2023-04-30 10:02:08 -0300

    Add Business Tab to Home Business

--- app/lib/pages/business_tabs/home_business.dart
+++ app/lib/pages/business_tabs/home_business.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lavanderiapro/auth/login.dart';
 import 'package:lavanderiapro/auth/register_business.dart';
+import 'package:lavanderiapro/pages/business_tabs/business_tab.dart';
 import 'package:lavanderiapro/pages/business_tabs/profile_business_tab.dart';
 import 'package:lavanderiapro/services/get_profile_service.dart';
 
@@ -46,7 +47,7 @@ class HomeBusinessTab extends StatelessWidget {
         body: const TabBarView(
           children: [
             ProfileBusinessTab(),
-            Text("NewOrderClientTab()"),
+            BusinessTab(),
             Text("OrdersClientTab()"),
           ],
         ),
