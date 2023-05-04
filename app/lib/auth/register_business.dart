ebdca5fdd7f8629eda0f517f296caa27f1b717c6
Santiago Mena <santiagomenape@gmail.com>
2023-05-04 01:18:51 -0300

    Redirect HomePage without set token

--- app/lib/auth/register_business.dart
+++ app/lib/auth/register_business.dart
@@ -98,7 +98,7 @@ class _RegisterBusinessPageState extends State<RegisterBusinessPage> {
                                       Navigator.push(
                                           context,
                                           MaterialPageRoute(
-                                              builder: (context) => HomePage(token: token!.token ?? '')
+                                              builder: (context) => HomePage()
                                           )
                                       ) :
                                   ScaffoldMessenger.of(context).showSnackBar(
