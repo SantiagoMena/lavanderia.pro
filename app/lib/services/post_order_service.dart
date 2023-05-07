d130784d28579ee0f7415e72663d672c1c3da833
Santiago Mena <santiagomenape@gmail.com>
2023-05-06 21:52:30 -0300

    WIP: Post Order Service

---
+++ app/lib/services/post_order_service.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:lavanderiapro/models/order.dart';
+import 'package:lavanderiapro/util/constants.dart';
+
+Future<ClientProfile?> postOrder(String token, OrderModel? order) async {
+  var url = Uri.http(API_HOST, 'business-order/${order!.businessId}');
+  var products = [];
+
+  order.getGrouped().forEach((element) {
+    products.add({
+      'id': element.id,
+      'amount': order.countProduct(element)
+    });
+  });
+
+  final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({
+        'products': products,
+        'address': {
+          'id': order.addressId
+        }
+      }
+  ));
+  if (response.statusCode == 200) {
+    print(response.body);
+    // Si la llamada al servidor fue exitosa, analiza el JSON
+    return ClientProfile.fromJson(json.decode(response.body));
+  } else {
+    // Si la llamada no fue exitosa, lanza un error.
+    // throw Exception('Failed to login');
+  }
+  return null;
+}
+
+class ClientProfile {
+  final String? id;
+  final String? name;
+  final String? createdAt;
+  final String? updatedAt;
+
+  ClientProfile({this.id, this.name, this.createdAt, this.updatedAt});
+
+  factory ClientProfile.fromJson(Map<String, dynamic> json) {
+    return ClientProfile(
+      id: json['id'],
+      name: json['name'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+    );
+  }
+}
